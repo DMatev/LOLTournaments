e9e27ecbeb57e8ba5c18b2cc296f3854183334d2
Plamen Kovachev <pckovachev@gmail.com>
2015-01-09 21:39:54 +0200

    Revert "front end"
    This reverts commit 5f992f3b6e51a419725aee378c9ed2a9f59f40bc.

--- app/config.js
+++ app/config.js
@@ -1,8 +1,7 @@
 module.exports = {
 	'jwtSecret' : 'i hate hakers',
 	'DB' : {
-		'url':'mongodb://ggeasy:swag@ds029911.mongolab.com:29911/tournaments'
-		//'url' : 'mongodb://localhost:27017/test'
+		'url' : 'mongodb://localhost:27017/test'
 	},
 	'server' : {
 		'name' : 'Tournaments'
